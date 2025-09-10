d6af88be1623ac8c6593b80b97a3744478e6416a
bademarc <zbademarc@gmail.com>
2025-09-11 03:08:24 +0500

    fix

--- hooks/useLeaderboard.tsx
+++ hooks/useLeaderboard.tsx
@@ -1,15 +1,14 @@
 "use client";
 
 import { useQuery } from "@tanstack/react-query";
-import { apiEndpoints } from "../lib/api";
-import axios from "axios";
+import { api, apiEndpoints } from "../lib/api";
 import { LeaderBoardData, LeaderboardResponse } from "../types";
 
 export function useLeaderboard(page: number) {
   return useQuery({
     queryKey: ["leaderboard", page], // Include page in query key
     queryFn: async (): Promise<LeaderboardResponse> => {
-      const { data } = await axios.get<LeaderboardResponse>(
+      const { data } = await api.get<LeaderboardResponse>(
         apiEndpoints.leaderBoard,
         { params: { page } }
       );
